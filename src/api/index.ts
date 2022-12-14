4fdea5729acf12cc8ac42344ac5caca236af3d4d
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-12-14 21:30:39 +0100

    feat: add utility to make search urls

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,25 @@
 import { MovieAPIResponse } from '../types/APIResponsesTypes'
 
-// api details
 export const {
   VITE_API_URL: API_URL,
   VITE_API_KEY: API_KEY,
   VITE_IMG_API_URL: IMG_API_URL,
 } = import.meta.env
 
-// fetch data
-export async function handleApiCall(url: string): Promise<MovieAPIResponse> {
+const defaultParams = `language=en-US&page=1&include_adult=false`
+
+export const makeSearchUrl = (searchTerm: string) => {
+  return `${API_URL}/search/movie?api_key=${API_KEY}&${defaultParams}&query=${searchTerm}`
+}
+
+export async function getApiData(url: string): Promise<MovieAPIResponse> {
+  console.log('fetching data...', { handleApiCall: url })
+
   let data
   try {
-    console.log('...fetching data..........')
     const res = await fetch(url)
     data = await res.json()
 
-    console.log({ handleApiCall: data })
     return data
   } catch (e) {
     console.error(e)
