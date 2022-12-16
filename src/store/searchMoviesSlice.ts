4b63ef86a496bc48343fd8ab4cb510845b0c2917
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-12-16 12:20:28 +0100

    feat: added async thunk to handle searching for movies

--- src/store/searchMoviesSlice.ts
+++ src/store/searchMoviesSlice.ts
@@ -1,18 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import camelcaseKeys from 'camelcase-keys'
 import { MovieDetails } from '../types/APIResponsesTypes'
+import { getApiData, makeSearchUrl } from '../utils/api'
+import { addDataIntoCache, getCacheData } from '../utils/cache'
 
-interface searchMoviesState {
+interface movieSearchState {
   data: [] | MovieDetails[]
   isLoading: true | false
 }
 
-const initialState: searchMoviesState = {
+const initialState: movieSearchState = {
   data: [],
   isLoading: true,
 }
 
-export const searchMoviesSlice = createSlice({
-  name: 'searchMovies',
+export const search = createAsyncThunk(
+  'movieSearch/search',
+  async (searchTerm: string) => {
+    const url = makeSearchUrl(searchTerm)
+
+    const cacheResponse = await getCacheData('search-cache', url)
+    console.log({ cacheResponse })
+    if (cacheResponse) return cacheResponse
+    else {
+      const apiResponse = await getApiData(url)
+
+      if (apiResponse && apiResponse.results.length) {
+        const data = camelcaseKeys(apiResponse.results)
+        addDataIntoCache('search-cache', url, data)
+
+        return data
+      }
+    }
+  }
+)
+
+export const movieSearchSlice = createSlice({
+  name: 'movieSearch',
   initialState,
   reducers: {
     addResults: (state, action) => {
@@ -22,4 +46,17 @@ export const searchMoviesSlice = createSlice({
       state.isLoading = action.payload
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(search.fulfilled, (state, action) => {
+      console.log(action)
+      if (action.payload) {
+        state.data = action.payload
+        state.isLoading = false
+      }
+    })
+  },
 })
+
+export const { addResults, setIsLoading } = movieSearchSlice.actions
+
+export default movieSearchSlice.reducer
