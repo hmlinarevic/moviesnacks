f638dd3ae79702de9a754e479d84deb6e2b0917b
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-12-20 11:30:38 +0100

    fix: revert file to previous commit

--- src/components/movie/MovieSearch.tsx
+++ src/components/movie/MovieSearch.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { searchMovies } from '../../api/searchMovies'
 import { useAppDispatch } from '../../hooks'
-import { search } from '../../store/movieSearchSlice'
+import { searchMoviesSlice } from '../../store/searchMoviesSlice'
 import './MovieSearch.css'
 
 const MovieSearch = () => {
@@ -9,18 +10,30 @@ const MovieSearch = () => {
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
 
+  const search = async () => {
+    navigate('/results')
+
+    const searchResult = await searchMovies(searchTerm)
+
+    if (!searchResult) {
+      dispatch(searchMoviesSlice.actions.addResults([]))
+    } else {
+      console.log({ searchResult })
+      dispatch(searchMoviesSlice.actions.addResults(searchResult))
+    }
+    dispatch(searchMoviesSlice.actions.setIsLoading(false))
+  }
+
   useEffect(() => {
     if (!searchTerm) return
 
-    const timeoutID = setTimeout(() => {
-      navigate('/results')
-      dispatch(search(searchTerm))
-    }, 1000)
+    const timeoutID = setTimeout(search, 1000)
 
     return () => {
       clearTimeout(timeoutID)
     }
 
+    // [navigate]
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchTerm])
 
@@ -32,6 +45,7 @@ const MovieSearch = () => {
         className="form-control search me-2"
         aria-label="Search movies"
         onChange={(e) => setSearchTerm(e.target.value)}
+        onSubmit={() => console.log('search submited')}
       />
     </>
   )
