b436f5dcd2c1baa195b5b66f06cd924dd6f64289
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-12-20 11:33:46 +0100

    refactor: start using async thunk

--- src/components/movie/MovieSearch.tsx
+++ src/components/movie/MovieSearch.tsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { searchMovies } from '../../api/searchMovies'
 import { useAppDispatch } from '../../hooks'
-import { searchMoviesSlice } from '../../store/searchMoviesSlice'
+import { search } from '../../store/movieSearchSlice'
 import './MovieSearch.css'
 
 const MovieSearch = () => {
@@ -10,30 +9,18 @@ const MovieSearch = () => {
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
 
-  const search = async () => {
-    navigate('/results')
-
-    const searchResult = await searchMovies(searchTerm)
-
-    if (!searchResult) {
-      dispatch(searchMoviesSlice.actions.addResults([]))
-    } else {
-      console.log({ searchResult })
-      dispatch(searchMoviesSlice.actions.addResults(searchResult))
-    }
-    dispatch(searchMoviesSlice.actions.setIsLoading(false))
-  }
-
   useEffect(() => {
     if (!searchTerm) return
 
-    const timeoutID = setTimeout(search, 1000)
+    const timeoutID = setTimeout(() => {
+      navigate('/results')
+      dispatch(search(searchTerm))
+    }, 1000)
 
     return () => {
       clearTimeout(timeoutID)
     }
 
-    // [navigate]
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchTerm])
 
@@ -45,7 +32,6 @@ const MovieSearch = () => {
         className="form-control search me-2"
         aria-label="Search movies"
         onChange={(e) => setSearchTerm(e.target.value)}
-        onSubmit={() => console.log('search submited')}
       />
     </>
   )
