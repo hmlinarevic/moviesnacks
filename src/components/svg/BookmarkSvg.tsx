fd2878e390ac5276a9221369c7271ddb163f13d6
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-12-20 10:22:41 +0100

    feat: add new svg

---
+++ src/components/svg/BookmarkSvg.tsx
@@ -0,0 +1,18 @@
+const BookmarkSvg = () => {
+  return (
+    <svg
+      // width="44"
+      // height="44"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+    </svg>
+  )
+}
+
+export default BookmarkSvg
