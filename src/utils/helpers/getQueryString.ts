a161709d750acd37c896bd1c9cccc759d5152df0
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-12-23 17:50:43 +0100

    feat: add helper

---
+++ src/utils/helpers/getQueryString.ts
@@ -0,0 +1,44 @@
+import { API_KEY } from '../api'
+
+export interface QueryParams {
+  [k: string]: string | number
+  // lang?: string
+  // page?: number
+}
+
+export const getQueryString = (path: string, params?: QueryParams): string => {
+  if (!params) return `?api_key=${API_KEY}`
+
+  const defaults = {
+    apiKey: `api_key=${API_KEY}`,
+    lang: `language=${params?.lang || 'en-US'}`,
+    page: `page=${params?.page || 1}`,
+  } as QueryParams
+
+  if (path !== '/search/movies') {
+    defaults.includeAdult = 'include_adult=false'
+  }
+
+  for (const key in params) {
+    if (defaults[key]) {
+      delete params[key]
+    }
+  }
+
+  const allParams = {
+    ...defaults,
+    ...params,
+  }
+
+  let queryString = '?'
+
+  for (const key in allParams) {
+    if (defaults[key]) {
+      queryString += `${allParams[key]}&`
+    } else {
+      queryString += `${key}=${allParams[key]}&`
+    }
+  }
+
+  return queryString.slice(0, -1)
+}
