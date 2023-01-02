3177eced5c782e981fa7c3ba64b78aaece11f081
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-01-02 20:44:22 +0100

    feat: add search movies function

--- src/store/asyncThunks.ts
+++ src/store/asyncThunks.ts
@@ -5,11 +5,37 @@ import { MovieDetails, MovieGenre } from '../types/APIResponsesTypes'
 import { getApiData, getApiEndpoint } from '../utils/api'
 import { addDataIntoCache, getCacheData } from '../utils/cache'
 
+// search slice
+export const searchMovies = createAsyncThunk(
+  'search/searchMovies',
+  async (serchTerm: string) => {
+    const endpoint = getApiEndpoint('/search/movie', { query: serchTerm })
+    console.log({ searching: true, endpoint })
+    const cacheResponse = await getCacheData<MovieDetails[]>(
+      'search-cache',
+      endpoint
+    )
+
+    if (cacheResponse) return cacheResponse
+    else {
+      const apiResponse = await getApiData(endpoint)
+      if (apiResponse?.results.length) {
+        const adapted = camelcaseKeys(apiResponse.results)
+
+        addDataIntoCache('search-cache', endpoint, adapted)
+
+        return adapted
+      }
+    }
+  }
+)
+
+// movies slice
 export const fetchMovieGenres = createAsyncThunk(
   'movies/fetchGenres',
   async () => {
     const endpoint = getApiEndpoint('/genre/movie/list')
-    const cacheResponse: MovieGenre[] | undefined = await getCacheData(
+    const cacheResponse = await getCacheData<MovieGenre[]>(
       'movie-genres',
       endpoint
     )
@@ -33,9 +59,9 @@ export const fetchPopularMovies = createAsyncThunk(
     let endpoint
 
     for (let currentPage = 1; currentPage <= pages; currentPage++) {
-      endpoint = getApiEndpoint('/movie/popular', currentPage)
+      endpoint = getApiEndpoint('/movie/popular', { page: currentPage })
 
-      const cacheResponse: MovieDetails[] | undefined = await getCacheData(
+      const cacheResponse = await getCacheData<MovieDetails[]>(
         'discover-movies',
         endpoint
       )
@@ -75,7 +101,7 @@ export const fetchNowPlayingMovies = createAsyncThunk(
   'movies/fetchNowPlaying',
   async () => {
     const endpoint = getApiEndpoint('/movie/now_playing')
-    const cacheResponse: MovieDetails[] | undefined = await getCacheData(
+    const cacheResponse = await getCacheData<MovieDetails[]>(
       'discover-movies',
       endpoint
     )
