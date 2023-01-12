4dedb0adf0f1d30f9c1e333b73cd2ed16314f42c
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-01-12 22:02:44 +0100

    feat: fetch data when app loads

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navigation from './components/ui/Navigation'
 import HomePage from './pages/HomePage'
 import DiscoveryPage from './pages/DiscoveryPage'
 import ResultsPage from './pages/ResultsPage'
 import MoviePage from './pages/MoviePage'
+import { useAppDispatch } from './hooks'
+import {
+  fetchMovieGenres,
+  fetchNowPlayingMovies,
+  fetchPopularMovies,
+} from './store/moviesSlice'
+import { loadFavoriteMovies } from './store/favoritesSlice'
 
 const App = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchMovieGenres())
+    dispatch(fetchPopularMovies(20)) // argument --> num of pages
+    dispatch(fetchNowPlayingMovies())
+
+    dispatch(loadFavoriteMovies())
+  }, [dispatch])
+
   return (
     <BrowserRouter>
       <Navigation />
