859d052303e0b6d4ad3ccd44659b2a9382b23959
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-01-18 21:18:14 +0100

    refactor: modified how to get api endpoint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,29 +1,23 @@
 import { MovieAPIResponse } from '../types/APIResponsesTypes'
+import { makeQueryString, QueryParams } from '../helpers/makeQueryString'
 
 export const {
-  VITE_API_URL: API_URL,
+  VITE_API_URL: API_BASE_URL,
   VITE_API_KEY: API_KEY,
   VITE_IMG_API_URL: IMG_API_URL,
 } = import.meta.env
 
-const defaultParams = `language=en-US&page=1&include_adult=false`
-
-export const getApiEndpoint = (resource: string) => {
-  return `${API_URL}/movie/${resource}?api_key=${API_KEY}&language=en-US`
-}
-
-export const makeSearchUrl = (searchTerm: string) => {
-  return `${API_URL}/search/movie?api_key=${API_KEY}&${defaultParams}&query=${searchTerm}`
+export const getApiEndpoint = (path: string, params?: QueryParams) => {
+  return `${API_BASE_URL}${path}${makeQueryString(path, params)}`
 }
 
 export async function getApiData(
-  url: string
+  endpoint: string
 ): Promise<MovieAPIResponse | undefined> {
-  console.log('fetching data...', { getApiData: url })
+  console.log('fetching data...', { getApiData: endpoint })
 
   try {
-    const res = await fetch(url)
-    console.log({ res })
+    const res = await fetch(endpoint)
 
     if (res.ok) {
       return res.json()
